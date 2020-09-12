c95be28edec85fab26452a6fa1bafe705372de52
Tomek <tompe97@tlen.pl>
2020-09-12 13:26:37 +0100

    Add content blocks & remove banner

--- wp-content/themes/tomtech/single-tutorial.php
+++ wp-content/themes/tomtech/single-tutorial.php
@@ -1,15 +1,27 @@
 <?php get_header(); ?>
 
-    <section class="post-banner tutorial-banner">
-        <div class="container flex flex-row align-center justify-center h-100">
-            <h1><?php the_title(); ?></h1>
-        </div>
-    </section>
-
     <main id="tutorial-content">
-        <div class="container">
+        <div class="container wider">
             <div class="content-wrapper">
-                <?php the_content(); ?>
+                <div id="intro">
+                    <h1><?php the_title(); ?></h1>
+                    <?php the_content(); ?>
+                </div>
+
+                <?php if (have_rows('steps')): ?>
+                    <?php $step_iteration = 1; ?>
+                    <?php while (have_rows('steps')): the_row(); ?>
+                        <?php
+                            $title        = get_sub_field('title');
+                            $instructions = get_sub_field('instructions');
+                        ?>
+
+                        <div class="step" id="step-<?php echo $step_iteration++; ?>">
+                            <h2><?php echo $title; ?></h2>
+                            <?php echo apply_filters('the_content', $instructions); ?>
+                        </div>
+                    <?php endwhile; ?>
+                <?php endif; ?>
             </div>
         </div>
     </main>
