949955bdf290c916857b552de6d26d2309d054f5
Tomek <tompe97@tlen.pl>
2020-09-29 20:23:33 +0100

    Add footer

--- wp-content/themes/tomtech/footer.php
+++ wp-content/themes/tomtech/footer.php
@@ -1,3 +1,14 @@
+</main> <!-- #site-content -->
+
+<footer>
+    <div class="container <?php echo is_single() ? 'wider' : ''; ?>">
+        <div class="content-wrapper">
+            <span>
+                <span><strong>My</strong>&nbsp;</span><a href="https://github.com/tomekpryjma" target="_blank" rel="noreferrer noopener">GitHub</a>
+            </span>
+        </div>
+    </div>
+</footer>
 
 <?php wp_footer(); ?>
 </body>
